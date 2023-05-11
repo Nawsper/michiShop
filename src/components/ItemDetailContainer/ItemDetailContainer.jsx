0e570ee74e0548138f6d3ee9a1619fa78922425f
Yasmin Burrut <nawsper@gmail.com>
2023-05-11 16:18:53 -0300

    Se crean productos con Firestore

--- src/components/ItemDetailContainer/ItemDetailContainer.jsx
+++ src/components/ItemDetailContainer/ItemDetailContainer.jsx
@@ -1,22 +1,41 @@
 import { useState, useEffect } from "react";
 import { ItemDetail } from "../ItemDetail/ItemDetail";
-import { callProductsById } from "../callProducts";
 import { useParams } from "react-router-dom";
+import { getDoc, doc } from "firebase/firestore";
+import { db } from "../../config/firebase";
 
 export const ItemDetailContainer = () => {
   const [product, setProduct] = useState(null);
+  const [loading, setLoading] = useState(true);
 
   const { itemId } = useParams();
 
   useEffect(() => {
-    callProductsById(itemId).then((response) => {
-      setProduct(response);
-    });
+    setLoading(true);
+
+    const docRef = doc(db, "products", itemId);
+
+    getDoc(docRef)
+      .then((response) => {
+        const data = response.data();
+        const productsAdapted = { id: response.id, ...data };
+        setProduct(productsAdapted);
+      })
+      .catch((error) => {
+        console.log(error);
+      })
+      .finally(() => {
+        setLoading(false);
+      });
   }, [itemId]);
 
   return (
     <div className="itemDetailContainer">
-      <ItemDetail {...product} />
+      {loading ? (
+        <p className="itemDetailContainer__p">Cargando producto...</p>
+      ) : (
+        <ItemDetail {...product} />
+      )}
     </div>
   );
 };
--- src/components/ItemListContainer/ItemListContainer.jsx
+++ src/components/ItemListContainer/ItemListContainer.jsx
@@ -1,34 +1,53 @@
 import { useEffect, useState } from "react";
 import "../../scss/components/ItemListContainer/_ItemListContainer.scss";
-import { callProducts, callProductsByCategory } from "../callProducts.js";
 import { ItemList } from "../ItemList/ItemList";
 import { useParams } from "react-router-dom";
+import { getDocs, collection, query, where } from "firebase/firestore";
+import { db } from "../../config/firebase";
 
 export const ItemListContainer = ({ greeting }) => {
   const [products, setProducts] = useState([]);
+  const [loading, setLoading] = useState(true);
 
   const { categoryId } = useParams();
 
   useEffect(() => {
-    const callFunc = categoryId ? callProductsByCategory : callProducts;
+    setLoading(true);
 
-    callFunc(categoryId)
-      .then((res) => {
-        setProducts(res);
+    const collectionRef = categoryId
+      ? query(collection(db, "products"), where("category", "==", categoryId))
+      : collection(db, "products");
+
+    getDocs(collectionRef)
+      .then((response) => {
+        const productsAdapted = response.docs.map((doc) => {
+          const data = doc.data();
+          return { id: doc.id, ...data };
+        });
+        setProducts(productsAdapted);
       })
       .catch((error) => {
-        console.error(error);
+        console.log(error);
+      })
+      .finally(() => {
+        setLoading(false);
       });
   }, [categoryId]);
 
   return (
     <div className="itemListContainer">
-      <h2 className="itemListContainer__h2">MichiProductos</h2>
-      <h4 className="itemListContainer__h4">{categoryId}</h4>
-      <p className="itemListContainer__p">{greeting}</p>
-      <div className="itemListContainer__div">
-        <ItemList products={products} />
-      </div>
+      {loading ? (
+        <p className="itemListContainer__p">Cargando producto...</p>
+      ) : (
+        <>
+          <h2 className="itemListContainer__h2">MichiProductos</h2>
+          <h4 className="itemListContainer__h4">{categoryId}</h4>
+          <p className="itemListContainer__p">{greeting}</p>
+          <div className="itemListContainer__div">
+            <ItemList products={products} />
+          </div>
+        </>
+      )}
     </div>
   );
 };
